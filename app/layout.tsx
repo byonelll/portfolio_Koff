4108786fbc6c0283f90192df485c41d8db040481
byonell <lucmercury4@gmail.com>
2025-07-30 20:54:14 +0100

    layout changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import ThemeToggle from './components/ThemeToggle';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
